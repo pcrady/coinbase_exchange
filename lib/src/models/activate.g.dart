72b7554a0099b16731350e5bd0cfd79d0b7d2f8a
Peter Crady <petercrady1@gmail.com>
2021-06-18 17:18:51 -0500

    add partfiles

---
+++ lib/src/models/activate.g.dart
@@ -0,0 +1,86 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'activate.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Activate _$ActivateFromJson(Map<String, dynamic> json) {
+  return Activate(
+    type: json['type'] as String?,
+    productId: json['product_id'] as String?,
+    timestamp: json['timestamp'] == null
+        ? null
+        : DateTime.parse(json['timestamp'] as String),
+    userId:
+        const StringToDoubleConverter().fromJson(json['user_id'] as String?),
+    profileId:
+        const StringToDoubleConverter().fromJson(json['profile_id'] as String?),
+    orderId: json['order_id'] as String?,
+    stopType: json['stop_type'] as String?,
+    side: _$enumDecodeNullable(_$CoinbaseSideEnumMap, json['side']),
+    stopPrice:
+        const StringToDoubleConverter().fromJson(json['stop_price'] as String?),
+    size: const StringToDoubleConverter().fromJson(json['size'] as String?),
+    funds: const StringToDoubleConverter().fromJson(json['funds'] as String?),
+    private: json['private'] as bool?,
+  );
+}
+
+Map<String, dynamic> _$ActivateToJson(Activate instance) => <String, dynamic>{
+      'type': instance.type,
+      'product_id': instance.productId,
+      'timestamp': instance.timestamp?.toIso8601String(),
+      'user_id': const StringToDoubleConverter().toJson(instance.userId),
+      'profile_id': const StringToDoubleConverter().toJson(instance.profileId),
+      'order_id': instance.orderId,
+      'stop_type': instance.stopType,
+      'side': _$CoinbaseSideEnumMap[instance.side],
+      'stop_price': const StringToDoubleConverter().toJson(instance.stopPrice),
+      'size': const StringToDoubleConverter().toJson(instance.size),
+      'funds': const StringToDoubleConverter().toJson(instance.funds),
+      'private': instance.private,
+    };
+
+K _$enumDecode<K, V>(
+  Map<K, V> enumValues,
+  Object? source, {
+  K? unknownValue,
+}) {
+  if (source == null) {
+    throw ArgumentError(
+      'A value must be provided. Supported values: '
+      '${enumValues.values.join(', ')}',
+    );
+  }
+
+  return enumValues.entries.singleWhere(
+    (e) => e.value == source,
+    orElse: () {
+      if (unknownValue == null) {
+        throw ArgumentError(
+          '`$source` is not one of the supported values: '
+          '${enumValues.values.join(', ')}',
+        );
+      }
+      return MapEntry(unknownValue, enumValues.values.first);
+    },
+  ).key;
+}
+
+K? _$enumDecodeNullable<K, V>(
+  Map<K, V> enumValues,
+  dynamic source, {
+  K? unknownValue,
+}) {
+  if (source == null) {
+    return null;
+  }
+  return _$enumDecode<K, V>(enumValues, source, unknownValue: unknownValue);
+}
+
+const _$CoinbaseSideEnumMap = {
+  CoinbaseSide.buy: 'buy',
+  CoinbaseSide.sell: 'sell',
+};
---
+++ lib/src/models/change.g.dart
@@ -0,0 +1,83 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'change.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Change _$ChangeFromJson(Map<String, dynamic> json) {
+  return Change(
+    type: json['type'] as String?,
+    time: json['time'] == null ? null : DateTime.parse(json['time'] as String),
+    sequence: json['sequence'] as int?,
+    orderId: json['order_id'] as String?,
+    productId: json['product_id'] as String?,
+    newSize:
+        const StringToDoubleConverter().fromJson(json['new_size'] as String?),
+    oldSize:
+        const StringToDoubleConverter().fromJson(json['old_size'] as String?),
+    newFunds:
+        const StringToDoubleConverter().fromJson(json['new_funds'] as String?),
+    oldFunds:
+        const StringToDoubleConverter().fromJson(json['old_funds'] as String?),
+    price: const StringToDoubleConverter().fromJson(json['price'] as String?),
+    side: _$enumDecodeNullable(_$CoinbaseSideEnumMap, json['side']),
+  );
+}
+
+Map<String, dynamic> _$ChangeToJson(Change instance) => <String, dynamic>{
+      'type': instance.type,
+      'time': instance.time?.toIso8601String(),
+      'sequence': instance.sequence,
+      'order_id': instance.orderId,
+      'product_id': instance.productId,
+      'new_size': const StringToDoubleConverter().toJson(instance.newSize),
+      'old_size': const StringToDoubleConverter().toJson(instance.oldSize),
+      'new_funds': const StringToDoubleConverter().toJson(instance.newFunds),
+      'old_funds': const StringToDoubleConverter().toJson(instance.oldFunds),
+      'price': const StringToDoubleConverter().toJson(instance.price),
+      'side': _$CoinbaseSideEnumMap[instance.side],
+    };
+
+K _$enumDecode<K, V>(
+  Map<K, V> enumValues,
+  Object? source, {
+  K? unknownValue,
+}) {
+  if (source == null) {
+    throw ArgumentError(
+      'A value must be provided. Supported values: '
+      '${enumValues.values.join(', ')}',
+    );
+  }
+
+  return enumValues.entries.singleWhere(
+    (e) => e.value == source,
+    orElse: () {
+      if (unknownValue == null) {
+        throw ArgumentError(
+          '`$source` is not one of the supported values: '
+          '${enumValues.values.join(', ')}',
+        );
+      }
+      return MapEntry(unknownValue, enumValues.values.first);
+    },
+  ).key;
+}
+
+K? _$enumDecodeNullable<K, V>(
+  Map<K, V> enumValues,
+  dynamic source, {
+  K? unknownValue,
+}) {
+  if (source == null) {
+    return null;
+  }
+  return _$enumDecode<K, V>(enumValues, source, unknownValue: unknownValue);
+}
+
+const _$CoinbaseSideEnumMap = {
+  CoinbaseSide.buy: 'buy',
+  CoinbaseSide.sell: 'sell',
+};

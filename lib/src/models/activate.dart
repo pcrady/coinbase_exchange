55da9c22d8b7bc65b0796d109f1762edf55aed6c
Peter Crady <petercrady1@gmail.com>
2021-06-18 17:18:20 -0500

    complete public feeds

--- lib/src/models/activate.dart
+++ lib/src/models/activate.dart
@@ -1,3 +1,45 @@
-class Activate {
+import 'package:coinbase_dart/coinbase_dart.dart';
+import 'package:coinbase_dart/src/lib/custom_json_converters.dart';
+import 'package:json_annotation/json_annotation.dart';
 
+part 'activate.g.dart';
+
+@JsonSerializable(fieldRename: FieldRename.snake, explicitToJson: true)
+class Activate {
+  final String? type;
+  final String? productId;
+  final DateTime? timestamp;
+  @StringToDoubleConverter()
+  final double? userId;
+  @StringToDoubleConverter()
+  final double? profileId;
+  final String? orderId;
+  final String? stopType;
+  final CoinbaseSide? side;
+  @StringToDoubleConverter()
+  final double? stopPrice;
+  @StringToDoubleConverter()
+  final double? size;
+  @StringToDoubleConverter()
+  final double? funds;
+  final bool? private;
+
+  Activate({
+    this.type,
+    this.productId,
+    this.timestamp,
+    this.userId,
+    this.profileId,
+    this.orderId,
+    this.stopType,
+    this.side,
+    this.stopPrice,
+    this.size,
+    this.funds,
+    this.private,
+  });
+
+  factory Activate.fromJson(Map<String, dynamic> json) => _$ActivateFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ActivateToJson(this);
 }
--- lib/src/models/change.dart
+++ lib/src/models/change.dart
@@ -1,3 +1,43 @@
-class Change {
+import 'package:coinbase_dart/coinbase_dart.dart';
+import 'package:coinbase_dart/src/lib/custom_json_converters.dart';
+import 'package:json_annotation/json_annotation.dart';
 
+part 'change.g.dart';
+
+@JsonSerializable(fieldRename: FieldRename.snake, explicitToJson: true)
+class Change {
+  final String? type;
+  final DateTime? time;
+  final int? sequence;
+  final String? orderId;
+  final String? productId;
+  @StringToDoubleConverter()
+  final double? newSize;
+  @StringToDoubleConverter()
+  final double? oldSize;
+  @StringToDoubleConverter()
+  final double? newFunds;
+  @StringToDoubleConverter()
+  final double? oldFunds;
+  @StringToDoubleConverter()
+  final double? price;
+  final CoinbaseSide? side;
+
+  Change({
+    this.type,
+    this.time,
+    this.sequence,
+    this.orderId,
+    this.productId,
+    this.newSize,
+    this.oldSize,
+    this.newFunds,
+    this.oldFunds,
+    this.price,
+    this.side
+  });
+
+  factory Change.fromJson(Map<String, dynamic> json) => _$ChangeFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ChangeToJson(this);
 }

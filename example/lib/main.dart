c5a1704b78efbca1bd5081bab36d3040f3a2368e
Peter Crady <petercrady1@gmail.com>
2021-10-12 19:58:25 -0500

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
-
+import 'package:candlesticks/candlesticks.dart' as graph;
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:coinbase_exchange/coinbase_exchange.dart' as cb;
 
 void main() {
@@ -12,12 +13,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Coinbase Exchange Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomePage(),
+      home: HomePage(),
     );
   }
 }
@@ -32,17 +30,28 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final _client = cb.WebsocketClient();
   final _streamController = StreamController.broadcast();
+  final oCcy = NumberFormat("#,##0.00", "en_US");
+  final _textStyle = const TextStyle(
+    fontSize: 28.0,
+    color: Colors.white70,
+  );
   var btcPrice = '';
   var ethPrice = '';
+  var ltePrice = '';
+  final cb.ProductsClient productsClient = cb.ProductsClient(
+    apiKey: '',
+    secretKey: '',
+    passphrase: '',
+  );
 
   @override
   void initState() {
     _client.connect();
     _streamController.addStream(
       _client.subscribe(
-        productIds: ['ETH-USD', 'BTC-USD'],
+        productIds: ['ETH-USD', 'BTC-USD', 'LTC-USD'],
         channels: [cb.CoinbaseChannel.ticker],
-      )!,
+      ),
     );
     super.initState();
   }
@@ -58,45 +67,100 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Coinbase Exchange Demo'),
+        backgroundColor: const Color(0xff113650),
+        title: const Text(
+          'Coinbase Exchange Demo',
+          style: TextStyle(color: Colors.white70),
+        ),
       ),
-      body: Center(
-        child: StreamBuilder<dynamic>(
-            stream: _streamController.stream,
-            builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-              if (snapshot.hasError) {
-                return Text(snapshot.error.toString());
-              }
-
-              if (snapshot.connectionState == ConnectionState.none) {
-                return const Text('None');
-              } else if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Text('Waiting...');
-              } else if (snapshot.connectionState == ConnectionState.active) {
-                var data = snapshot.data;
-                if (data is cb.StreamTicker) {
-                  if (data.productId == 'ETH-USD') {
-                    ethPrice = data.price.toString();
-                  } else {
-                    btcPrice = data.price.toString();
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Color(0xff1d3f64),
+            Color(0xff62467d),
+          ],
+        )),
+        child: Center(
+          child: Column(
+            children: [
+              FutureBuilder(
+                future: productsClient.getProductCandles(
+                  productId: 'BTC-USD',
+                ),
+                builder: (BuildContext context, AsyncSnapshot<List<cb.Candle>> snapshot) {
+                  if (snapshot.hasData) {
+                    List<graph.Candle> candles = snapshot.data!.map((e) => graph.Candle(
+                      date: e.time!,
+                      high: e.high!,
+                      low: e.low!,
+                      open: e.open!,
+                      close: e.close!,
+                      volume: e.volume!,
+                    )).toList();
+                    return AspectRatio(
+                      aspectRatio: 1.2,
+                      child: graph.Candlesticks(
+                        candles: candles,
+                        onIntervalChange: (_) async {},
+                        interval: '1m',
+                      ),
+                    );
                   }
+                  return Container();
+                },
+              ),
+              const SizedBox(height: 28.0),
+              StreamBuilder<dynamic>(
+                stream: _streamController.stream,
+                builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.none) {
+                    return const Text('None');
+                  } else if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Text('Waiting...');
+                  } else if (snapshot.connectionState == ConnectionState.active) {
+                    var data = snapshot.data;
+                    if (data is cb.StreamTicker) {
+                      if (data.productId == 'ETH-USD') {
+                        ethPrice = '\$' + oCcy.format(data.price);
+                      } else if (data.productId == 'LTC-USD') {
+                        ltePrice = '\$' + oCcy.format(data.price);
+                      } else {
+                        btcPrice = '\$' + oCcy.format(data.price);
+                      }
 
-                  return Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('Bitcoin price: $btcPrice'),
-                      const SizedBox(height: 12.0),
-                      Text('Etherium Price $ethPrice'),
-                    ],
-                  );
-                } else {
-                  return const Text('Something else');
-                }
-              } else if (snapshot.connectionState == ConnectionState.done) {
-                return const Text('Done');
-              }
-              return Container();
-            }),
+                      return Column(
+                        children: [
+                          Text(
+                            'Bitcoin Price: $btcPrice',
+                            style: _textStyle,
+                          ),
+                          const SizedBox(height: 18.0),
+                          Text(
+                            'Litecoin Price: $ltePrice',
+                            style: _textStyle,
+                          ),
+                          const SizedBox(height: 18.0),
+                          Text(
+                            'Etherium Price $ethPrice',
+                            style: _textStyle,
+                          ),
+                        ],
+                      );
+                    } else {
+                      return const Text('Something Else');
+                    }
+                  } else if (snapshot.connectionState == ConnectionState.done) {
+                    return const Text('Done');
+                  }
+                  return Container();
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
